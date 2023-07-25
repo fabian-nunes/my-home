de23a3f3e962c85e7ea49111735a161cad91001c
fabian-nunes <fabiannunes12@gmail.com>
2023-07-25 18:57:55 +0100

    Add navbar

---
+++ src/Navbar.js
@@ -0,0 +1,36 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+
+import logo from './img/logo.png';
+
+function HNavbar() {
+    return (
+        <>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img
+                            src={logo}
+                            width="30px"
+                            height="30px"
+                            className="d-inline-block align-top"
+                            alt="Smart Home"
+                        />{' Smart Home '}
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#history">History</Nav.Link>
+                        </Nav>
+                        <Button variant="outline-danger">Logout</Button>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+}
+
+export default HNavbar;
