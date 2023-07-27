1836319e199b775123b89ce91aa130561c0c81ab
fabian-nunes <fabiannunes12@gmail.com>
2023-07-27 08:24:02 +0100

    Receive data from the API

--- src/components/Sensor.jsx
+++ src/components/Sensor.jsx
@@ -7,9 +7,10 @@ class Sensor extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            temperature: 0,
-            humidity: 0
-        };
+            name: this.props.name,
+            value: 0,
+            time: new Date().toLocaleTimeString(),
+        }
     }
 
     render() {
@@ -17,9 +18,9 @@ class Sensor extends Component {
             <>
                 <Card className="text-center">
                     {this.props.name === "Temperature" ? (
-                        <Card.Header className="fw-bold" style={{backgroundColor: '#93baff'}}>{this.props.name}: {this.state.temperature}</Card.Header>
+                        <Card.Header className="fw-bold" style={{backgroundColor: '#93baff'}}>{this.props.name}: {this.state.value}</Card.Header>
                     ) : (
-                        <Card.Header className="fw-bold" style={{backgroundColor: '#ffbf6c'}}>{this.props.name}: {this.state.humidity}</Card.Header>
+                        <Card.Header className="fw-bold" style={{backgroundColor: '#ffbf6c'}}>{this.props.name}: {this.state.value}</Card.Header>
                     )}
                     <Card.Body>
                         {this.props.name === "Temperature" ? (
@@ -29,11 +30,33 @@ class Sensor extends Component {
                         )}
                     </Card.Body>
                     <Card.Footer>
-                        <p className="m-0"><b>Update:</b></p>
+                        <p className="m-0"><b>Update:</b> {this.state.time}</p>
                     </Card.Footer>
                 </Card>
             </>
         );
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            fetch("http://localhost:5000/api/sensor/data?name="+this.props.name)
+                .then(res => res.json())
+                .then(data => {
+                    let dateO = new Date(data.createdAt);
+                    let dateT = dateO.toLocaleTimeString();
+                    let dateD = dateO.toLocaleDateString();
+                    let dateL = dateD + " " + dateT;
+                    this.setState({
+                        value: data.value,
+                        //convert to local time
+                        time: dateL,
+                    });
+                });
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
     }
 }
 
